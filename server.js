adaa7082f0683175ce577da2890680368cb0b606
marijan10marijan <mmarijan39@gmail.com>
2024-03-17 21:28:44 +0100

    new changes

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import cors from "cors";
-import router from "./routes/records.js";
+import { ObjectId } from "mongodb";
+import db from "../db/connection.js";
 
 const PORT = process.env.PORT || 5050;
 const app = express();
@@ -8,11 +9,70 @@ const app = express();
 // Middleware
 app.use(cors()); // Enable CORS
 app.use(express.json());
-app.use("/record", router);
 
 // Routes
-app.get("/", (req, res) => {
-  res.send("Hello, world!");
+// GET ALL RECORDS ************************************
+app.get("/", async (req, res) => {
+  const collection = await db.collection("records").find({}).toArray();
+  res.send(collection).status(200);
+});
+
+// GET A SINGLE RECORD ************************************
+app.get("/:id", async (req, res) => {
+  const collection = await db
+    .collection("records")
+    .findOne({ _id: new ObjectId(req.params.id) });
+  if (!collection) return res.send("Record not found").status(404);
+  else return res.send(collection).status(200);
+});
+
+// CREATE A NEW RECORD ************************************
+app.post("/", async (req, res) => {
+  try {
+    let newDocument = {
+      name: req.body.name,
+      position: req.body.position,
+      gender: req.body.gender,
+      level: req.body.level,
+    };
+    const collection = await db.collection("records").insertOne(newDocument);
+    res.send(collection).status(201);
+  } catch (error) {
+    console.error(error);
+    res.send(error + "Error").status(500);
+  }
+});
+
+// UPDATE A RECORD ************************************
+app.patch("/:id", async (req, res) => {
+  try {
+    const query = { _id: new ObjectId(req.params.id) };
+    const updates = {
+      $set: {
+        name: req.body.name,
+        position: req.body.position,
+        level: req.body.level,
+      },
+    };
+    let collection = await db.collection("records").updateOne(query, updates);
+    res.send(collection).status(200);
+  } catch (error) {
+    console.error(error);
+    res.send(error + "Error").status(500);
+  }
+});
+
+// DELETE A RECORD ************************************
+app.delete("/:id", async (req, res) => {
+  try {
+    const collection = await db
+      .collection("records")
+      .deleteOne({ _id: new ObjectId(req.params.id) });
+    res.send(collection).status(200);
+  } catch (error) {
+    console.error(error);
+    res.send(error + "Error").status(500);
+  }
 });
 
 // Start the server
