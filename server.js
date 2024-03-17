90f774ab71067d5ef3540c9a0de0b6e41c887497
marijan10marijan <mmarijan39@gmail.com>
2024-03-17 21:31:00 +0100

    new changes

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 import express from "express";
 import cors from "cors";
 import { ObjectId } from "mongodb";
-import db from "../db/connection.js";
+import db from "./db/connection";
 
 const PORT = process.env.PORT || 5050;
 const app = express();
