5a259b14a96f3b8782a3cc793c6e1c595bfa4973
marijan10marijan <mmarijan39@gmail.com>
2024-03-17 21:16:23 +0100

    new changes

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 import express from "express";
 import cors from "cors";
-import records from "./routes/records.js";
+import router from "./routes/records.js";
 
 const PORT = process.env.PORT || 5050;
 const app = express();
@@ -8,7 +8,7 @@ const app = express();
 // Middleware
 app.use(cors()); // Enable CORS
 app.use(express.json());
-app.use("/record", records);
+app.use("/record", router);
 
 // Routes
 app.get("/", (req, res) => {
