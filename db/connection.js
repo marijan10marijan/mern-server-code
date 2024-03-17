e28c4ebf3892136cf993ace999a7eee191440032
marijan10marijan <mmarijan39@gmail.com>
2024-03-17 20:57:34 +0100

    finished mern server code

---
+++ db/connection.js
@@ -0,0 +1,21 @@
+import { MongoClient, ServerApiVersion } from "mongodb";
+
+const uri = process.env.ATLAS_URI || "";
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+try {
+  await client.connect();
+  await client.db("admin").command({ ping: 1 });
+  console.log("Connected to the database. Pinged your deployment.");
+} catch (error) {
+  console.error(error);
+}
+
+let db = client.db("employees");
+export default db;
---
+++ server.js
@@ -0,0 +1,21 @@
+import express from "express";
+import cors from "cors";
+import records from "./routes/records.js";
+
+const PORT = process.env.PORT || 5050;
+const app = express();
+
+// Middleware
+app.use(cors()); // Enable CORS
+app.use(express.json());
+app.use("/record", records);
+
+// Routes
+app.get("/", (req, res) => {
+  res.send("Hello, world!");
+});
+
+// Start the server
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
